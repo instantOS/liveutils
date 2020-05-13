a812cda7372c98b299bb374b3cc4c3acc5d2020b
paperbenni <paperbenni@gmail.com>
2020-05-13 19:33:34 +0200

    add status report

--- instantosinstaller.sh
+++ instantosinstaller.sh
@@ -1,4 +1,57 @@
 #!/bin/bash
 # instantOS installer wrapper
 
-curl -Ls git.io/instantarch | sudo bash
+# allow disabling gui
+if [ -n "$1" ] && [ "$1" = "-cli" ]; then
+    curl -Ls git.io/instantarch | sudo bash
+    exit
+fi
+
+prep conky && pkill conky
+
+# open up a logging window on second tag
+echo "beginning installation" >/root/instantos.log
+xdotool key super+2
+urxvt -e bash -c "sudo tail -f /root/instantos.log"
+sleep 3
+xdotool key super+1
+
+# run actual installer
+curl -Ls git.io/instantarch | sudo bash &>/root/instantos.log &
+
+# status bar showing install progress
+while :; do
+    if [ -e /opt/finishinstall ]; then
+        pkill instantmenu
+        break
+    fi
+
+    if ! [ -e /opt/instantprogress ]; then
+        sleep 1
+        continue
+    fi
+
+    if [ -z "$INSTANTPROGRESS" ]; then
+        INSTANTPROGRESS="$(cat /opt/instantprogress)"
+    fi
+
+    NEWINSTANTPROGRESS="$(cat /opt/instantprogress)"
+    if ! [ "$INSTANTPROGRESS" = "$NEWINSTANTPROGRESS" ]; then
+        INSTANTPROGRESS="$NEWINSTANTPROGRESS"
+        pkill instantmenu
+    fi
+    sleep 2
+
+done &
+
+while :; do
+    if [ -e /opt/finishinstall ]; then
+        break
+    fi
+
+    if [ -n "$INSTANTPROGRESS" ]; then
+        echo "> $INSTANTPROGRESS" | instantmenu -y 34 -l 1 -G
+    fi
+    sleep 1
+
+done
