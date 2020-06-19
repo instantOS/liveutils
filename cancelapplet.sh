cfb81bd128b45303db777c5559f4153ce3455112
paperbenni <paperbenni@gmail.com>
2020-06-19 19:40:16 +0200

    kill userinstallpid

--- cancelapplet.sh
+++ cancelapplet.sh
@@ -6,11 +6,19 @@ killinstallation() {
     if sudo ps aux | grep '^root' | grep 'ask.sh' || [ "$1" = "force" ]; then
         sudo ps aux | grep '^root' | grep -Ei '(ask\.sh|instantosinstaller|sudo|install)' |
             grep -o 'root[^a-z0-9]*[0-9]*' | grep -o '[0-9]*' >/tmp/installpid
+        sudo ps aux | grep -Ei 'instantosinstaller' |
+            grep -o 'root[^a-z0-9]*[0-9]*' | grep -o '[0-9]*' >/tmp/userinstallpid
 
         while read p; do
             echo "$p"
             sudo kill "$p"
         done </tmp/installpid
+
+        while read p; do
+            echo "$p"
+            sudo kill "$p"
+        done </tmp/userinstallpid
+
         pkill instantmenu
         sudo pkill instantmenu
     fi
