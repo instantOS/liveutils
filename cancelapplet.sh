ab400704230dac1e5fbb905e6e43c717062fd5a7
paperbenni <paperbenni@gmail.com>
2020-06-19 19:32:26 +0200

    imrove cancel applet

--- cancelapplet.sh
+++ cancelapplet.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-# little bottom applet that is able to kill the installatio
+# little bottom applet that is able to kill the installation
+
+killinstallation() {
+    if sudo ps aux | grep '^root' | grep 'ask.sh' || [ "$1" = "force" ]; then
+        sudo ps aux | grep '^root' | grep -Ei '(ask\.sh|instantosinstaller|sudo|install)' |
+            grep -o 'root[^a-z0-9]*[0-9][0-9][0-9][0-9]' | grep -o '[0-9]*' >/tmp/installpid
+
+        while read p; do
+            echo "$p"
+            sudo kill "$p"
+        done </tmp/installpid
+    fi
+}
 
 while :; do
 
@@ -8,7 +20,7 @@ while :; do
     if echo "yes
 no" | instantmenu -c -G -l 2 -w 500 -p "do you want to cancel the installation" |
         grep -q yes; then
-        sudo pkill instantosinstaller
+        killinstallation
         exit
     fi
 
