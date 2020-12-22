a7ae89a84e56fbf75d8be8d012e95804510633f4
paperbenni <paperbenni@gmail.com>
2020-12-22 17:25:02 +0100

    fix cowspace

--- liveautostart.sh
+++ liveautostart.sh
@@ -6,13 +6,15 @@ echo "applying live session tweaks"
 # increase tmpfs size depending on ram size
 MEMSIZERAW="$(grep MemTotal /proc/meminfo | grep -o '[0-9]*' | head -1)"
 
-if grep -Eq '.{8,}'; then
-    GIGSIZE="$(sed 's/........{8,}$//g' <<<"$MEMSIZERAW")"
+if grep -Eq '.{7,}'; then
+    GIGSIZE="$(sed 's/......$//g' <<<"$MEMSIZERAW")"
     echo "$GIGSIZE gigs of ram detected"
     if [ "$GIGSIZE" -gt 1 ]; then
         TMPSIZE="$((GIGSIZE / 2))"
     fi
     if [ "$TMPSIZE" -eq "$TMPSIZE" ] && [ "$TMPSIZE" -gt 0 ]; then
+        echo "numbers look fine"
+    else
         echo "failed setting tmpfs size"
         exit
     fi
@@ -20,6 +22,7 @@ if grep -Eq '.{8,}'; then
     mount -o remount,size="${TMPSIZE}G" /run/archiso/cowspace
 
 else
+    echo "defaulting to 512Mb of cowspace"
     mount -o remount,size=512M /run/archiso/cowspace
 fi
 
